81081809c8f6de2ca3885501c9862da32486da59
Alex13113 <60936183+Alex13113@users.noreply.github.com>
2020-02-18 21:30:48 +0300

    Updates

---
+++ precache-manifest.f046898b13cf8df7e3c3b5a3da954ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd4e0f171326a0d193a44174d3f5aef",
+    "url": "//Alex13113github.io/NAK-Coin/index.html"
+  },
+  {
+    "revision": "6cd59f9b250534eb896b",
+    "url": "//Alex13113github.io/NAK-Coin/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "757e811d802504a70bfc",
+    "url": "//Alex13113github.io/NAK-Coin/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6cd59f9b250534eb896b",
+    "url": "//Alex13113github.io/NAK-Coin/static/js/2.6e79a4ec.chunk.js"
+  },
+  {
+    "revision": "757e811d802504a70bfc",
+    "url": "//Alex13113github.io/NAK-Coin/static/js/main.fc7b7b84.chunk.js"
+  },
+  {
+    "revision": "7e9e91e9bd29de2eb132",
+    "url": "//Alex13113github.io/NAK-Coin/static/js/runtime-main.1b88a634.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "//Alex13113github.io/NAK-Coin/static/media/persik.4e1ec840.png"
+  }
+]);
